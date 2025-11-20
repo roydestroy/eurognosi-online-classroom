ad9209e3c96c0ca29ab72b9720b38a0e5e5af111
Panagiotis <panos_ned@yahoo.gr>
2025-11-20 10:33:28 +0200

    Fixed updater.

--- DailyDesktopApp/App.xaml.cs
+++ DailyDesktopApp/App.xaml.cs
@@ -1,8 +1,20 @@
-﻿using System.Windows;
+﻿using System.Threading.Tasks;
+using System.Windows;
 
 namespace DailyDesktopApp
 {
     public partial class App : Application
     {
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Fire and forget – if it fails, the app still starts
+            _ = UpdateService.CheckForUpdatesAsync();
+
+            // then show your main window
+            var main = new MainWindow();
+            main.Show();
+        }
     }
 }
