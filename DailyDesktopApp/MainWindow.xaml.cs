85b8b27ae05f0f4266d511e75388115eed6206c5
Panagiotis <panos_ned@yahoo.gr>
2025-11-21 16:23:46 +0200

    Removed unused dependencies

--- DailyDesktopApp/MainWindow.xaml.cs
+++ DailyDesktopApp/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers; // <-- NEW
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
