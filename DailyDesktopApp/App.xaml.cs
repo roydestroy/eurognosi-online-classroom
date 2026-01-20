60a82ab84f063eab3b75b984c8ffa98565fa82e6
Panagiotis <panos_ned@yahoo.gr>
2026-01-20 10:38:21 +0200

    Application now allows only 1 instance to run at a time.

--- DailyDesktopApp/App.xaml.cs
+++ DailyDesktopApp/App.xaml.cs
@@ -1,12 +1,28 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace DailyDesktopApp
 {
     public partial class App : Application
     {
+        private const string MutexName = "Global\\DailyDesktopApp_SingleInstance";
+        private static Mutex? _mutex;
+
         protected override async void OnStartup(StartupEventArgs e)
         {
+            // ---- Single-instance gate ----
+            bool createdNew;
+            _mutex = new Mutex(initiallyOwned: true, name: MutexName, createdNew: out createdNew);
+
+            if (!createdNew)
+            {
+                // Another instance is already running -> exit quietly
+                Shutdown();
+                return;
+            }
+            // ------------------------------
+
             base.OnStartup(e);
 
             // Optional: background update check
@@ -18,6 +34,21 @@ namespace DailyDesktopApp
 
             var main = new MainWindow();
             main.Show();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            try
+            {
+                _mutex?.ReleaseMutex();
+                _mutex?.Dispose();
+            }
+            catch
+            {
+                // ignore
+            }
+
+            base.OnExit(e);
         }
     }
 }
